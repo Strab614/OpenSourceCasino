ded25283c2e27e38d37bfaf0f7cbdf313832d57e
Strab614 <strab614@outlook.com>
2025-07-03 16:05:46 -0400

    Fix API Base URL for Network Error

--- src/stores/algorand.js
+++ src/stores/algorand.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = '/api';
 
 export const useAlgorandStore = defineStore('algorand', {
   state: () => ({
